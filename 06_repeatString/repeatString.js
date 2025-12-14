f2daf40518a6138771e4f01f55db22964d583cb0
Alex <agscheaua@gmail.com>
2025-12-14 22:47:41 +0200

    06_repeatString - done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num){
+    let greeting = "";  
+    for (i = 1; i <= num; i++){
+        greeting += str;
+    }
+    console.log(greeting);
+    return greeting;
 };
-
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
+   
