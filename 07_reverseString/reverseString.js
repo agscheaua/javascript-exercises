aa4d944762b469d58abe8c5a2a8a5b076e706ad8
Alex <agscheaua@gmail.com>
2025-12-15 21:14:08 +0200

    07_reverseString - done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+function reverseString(paraString){
+    let splitStr = paraString.split('');
+    let reverseSplitStr = splitStr.reverse();
+    let joinedReverseSplitStr = reverseSplitStr.join("");
+    console.log(joinedReverseSplitStr);
+    return joinedReverseSplitStr;
+}
+reverseString("hello there"); 
 
 // Do not edit below this line
 module.exports = reverseString;
