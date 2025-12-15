8b1252da696170442d67f1b649fce874580d8783
Alex <agscheaua@gmail.com>
2025-12-15 22:51:54 +0200

    09_sumAll - done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+function sumAll(para1, para2){
+   let sumOfAll = 0;
+   for (i = para1; i <= para2; i++){
+        sumOfAll += i; 
+   }
+   return sumOfAll; 
 
 };
+console.log(sumAll(2, 44)); 
+     
+     
+     
 
 // Do not edit below this line
 module.exports = sumAll;
