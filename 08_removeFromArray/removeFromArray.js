fdd64202b7b0cfb3f607ede36f9c7bc84e3b82c7
Alex <agscheaua@gmail.com>
2025-12-15 21:57:54 +0200

    08_removeFromArray - done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+
+function removeFromArray(para1, para2){
+    let para1Filtered = para1.filter((item) => {
+        if (item !== 3){
+            return item;
+        }
+        else{}
+    });
+    if (para2 !== 3){
+        return para2;
+    }
+    else{}  
+    return para1Filtered;                     
 };
+console.log(removeFromArray([1,2,3,4], 3));    
+  
+  
 
 // Do not edit below this line
 module.exports = removeFromArray;
