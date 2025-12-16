609002c2bc8e385746341214e2c11d34a3ce9229
Alex <agscheaua@gmail.com>
2025-12-16 22:46:50 +0200

    10_leapYears - done

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
-
-};
+function leapYears(para1){
+    if (para1 % 4 === 0){
+        console.log(`${para1}, is a leap year!`);
+        return true;
+    }
+    else { 
+        console.log(`${para1}, is not a leap year!`);
+        return false;
+    };
+    if (para1 % 100 === 0 && para1 % 400 === 0){
+        console.log(`${para1}, is a leap year!`);
+        return true;
+    }
+    else {
+        console.log(`${para1}, is not a leap year!`);
+        return false;
+    };
+}
+leapYears(1985);      
+      
 
 // Do not edit below this line
 module.exports = leapYears;
