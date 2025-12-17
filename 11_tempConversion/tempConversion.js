bbc57eb3cd5ea46569e022c6f78d3eb721c09d39
Alex <agscheaua@gmail.com>
2025-12-17 21:30:09 +0200

    11_tempConversion - done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+// celsius = (fahrenheit - 32) * 9 / 5   
+function convertToCelsius (fahrenheitTemp) {
+    let celsiusConversion = (fahrenheitTemp -32) * 5/9;
+    celsiusConversion = (Math.round(celsiusConversion * 10) / 10);
+    console.log(celsiusConversion);
+    return celsiusConversion;  
 };
 
-const convertToFahrenheit = function() {
-};
+// fahrenheit = (celsius * (9/5) ) + 32
+function convertToFahrenheit (celsiusTemp) {
+    let fahrenheitConversion = (celsiusTemp * 9/5) + 32;
+    fahrenheitConversion = ((Math.round(fahrenheitConversion * 10) / 10));
+    console.log(fahrenheitConversion);
+    return fahrenheitConversion;
+};     
+convertToCelsius(100);    
+convertToFahrenheit(0); 
+       
 
 // Do not edit below this line
 module.exports = {
