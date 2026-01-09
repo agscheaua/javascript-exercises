c97f7eab055428452bc2a9f0e90b21eae58b05ee
Alex <agscheaua@gmail.com>
2026-01-09 21:12:06 +0200

    13 - palindromes - done, i reverse the word and i transformed the letters in array items, then i use a loop to merge them backwords in a variable the use the if...else to compare them.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (para1) {
+  const splitWord = para1.split('');
+  const splitWordLengthArray = splitWord.length - 1;
+  let reverseSplitWord = '';
+  for (let i = splitWordLengthArray; 0 <= i; i--) {
+    reverseSplitWord += splitWord[i];
+  };
+  if (para1 === reverseSplitWord) {
+    return true;
+  }
+  else {
+    return false;
+  };
 };
+
+console.log(palindromes("racecar"));
 
 // Do not edit below this line
 module.exports = palindromes;
