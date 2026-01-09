cb554f6d555e6c9994341e166fe7697300f4f52e
Alex <agscheaua@gmail.com>
2026-01-09 22:33:08 +0200

    14 fibonacci - done, this variant will sustain la ter modification

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(para1) {
+  let myArray = [1];
+  for (let i = 1; i < 100; i++) {
+    if (myArray.length < 2) {
+      myArray.push(i);
+    }
+    else {
+      myArray.push(getLastNr() + getSecondLastNr()); 
+    };
+  };
 
+  function getLastNr () {
+    let myArrayLastNr = myArray[myArray.length - 1];
+    return myArrayLastNr;
+  };
+  function getSecondLastNr () {
+    let myArraySecondLastNr = myArray[myArray.length - 2];
+    return myArraySecondLastNr;
+  };
+
+  console.log(myArray); 
+
+  return myArray[para1 - 1];
 };
+console.log(fibonacci(246));  
 
 // Do not edit below this line
 module.exports = fibonacci;
